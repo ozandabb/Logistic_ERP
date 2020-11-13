ae750a692e34bd46428f6e3ce8dc2d14191c1e60
ozandabb <osanda.gamage.79@gmail.com>
2020-11-13 09:28:27 +0530

    DB change

--- src/Controllers/Config.controller.js
+++ src/Controllers/Config.controller.js
@@ -4,9 +4,10 @@ import { confirmAlert } from "react-confirm-alert";
 class Config{
   constructor(){
     //backend server details
-    this.host = "http://localhost:";
-    // this.host = "http://saleserp-env.eba-u2mkdt2x.us-east-2.elasticbeanstalk.com";
-    this.port = "4000";
+    // this.host = "http://localhost:";
+    this.host = "http://saleserp-env.eba-u2mkdt2x.us-east-2.elasticbeanstalk.com";
+    // this.port = "4000";
+    this.port = "";
     this.password = "";
   }
 
